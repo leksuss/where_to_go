844d8e4fbb178c5e5e7841d421586fb76b44361a
Andrey Alekseev <leksus@leksus.info>
2023-06-04 11:27:35 +0300

    can upload images from place page in admin

--- places/admin.py
+++ places/admin.py
@@ -3,5 +3,14 @@ from django.contrib import admin
 # Register your models here.
 from .models import Places, Images
 
-admin.site.register(Places)
+
+class ImagesInline(admin.TabularInline):
+    model = Images
+    extra = 1
+
+@admin.register(Places)
+class MasterAdmin(admin.ModelAdmin):
+    inlines = (ImagesInline,)
+
+
 admin.site.register(Images)
