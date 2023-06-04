c9047939a9df2611acc24d72783cc7d09b331c3e
Andrey Alekseev <leksus@leksus.info>
2023-06-04 12:22:40 +0300

    add preview for imaages in place admin page

--- places/admin.py
+++ places/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
 # Register your models here.
 from .models import Places, Images
@@ -6,10 +7,15 @@ from .models import Places, Images
 
 class ImagesInline(admin.TabularInline):
     model = Images
+    readonly_fields = ['preview_image']
     extra = 1
 
+    def preview_image(self, obj):
+        return format_html(f'<img src="{obj.image.url}" width="200">')
+
+
 @admin.register(Places)
-class MasterAdmin(admin.ModelAdmin):
+class PlacesAdmin(admin.ModelAdmin):
     inlines = (ImagesInline,)
 
 
