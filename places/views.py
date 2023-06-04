1ec0ab0247b1ee9ac07439db49d19c43b0d3b578
Andrey Alekseev <leksus@leksus.info>
2023-06-04 10:43:39 +0300

    get data from DB to view

--- places/views.py
+++ places/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Places, Images
+
+def index(request):
+
+    places_geo_json = {
+        'type': 'FeatureCollection',
+        'features': [],
+    }
+    places = Places.objects.all()
+    for place in places:
+        place_slug = None
+        if place.id == 2:
+            place_slug = 'roofs24'
+        elif place.id == 1:
+            place_slug = 'moscow_legends'
+        places_geo_json['features'].append({
+              'type': 'Feature',
+              'geometry': {
+                'type': 'Point',
+                'coordinates': [place.coordinate_lng, place.coordinate_lat]
+              },
+              'properties': {
+                'title': place.title,
+                'placeId': place_slug,
+                'detailsUrl': f'https://raw.githubusercontent.com/devmanorg/where-to-go-frontend/master/places/{place_slug}.json'
+              }
+        })
+    context = {
+        'places': places_geo_json,
+    }
+
+    return render(request, 'index.html', context=context)
--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -18,10 +18,10 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
-from where_to_go import views
+from places import views as places_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index),
+    path('', places_views.index),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
   + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
