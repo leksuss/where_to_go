8339235b7c798259d2dd82a38c9d2d7099ce9ea2
Andrey Alekseev <leksus@leksus.info>
2023-06-06 16:57:40 +0300

    move sensitive settings to .env

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -13,6 +13,11 @@ import os
 
 from pathlib import Path
 
+from environs import Env
+
+env = Env()
+env.read_env()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,13 +26,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7wc4gr))o^ef-vheavk1nj&!#+(poj2_4rhbt-o^pgniv(&&4o'
+SECRET_KEY = env.str('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool('DEBUG')
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
 # Application definition
 
